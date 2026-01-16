59c5f3775de0e070e184405115b8f48e33391ae1
James <jamesagbonz@gmail.com>
2026-01-16 11:01:55 -0500

    Migrate to new repository with updated paths for Mywebsite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MYportfolio.github.io/',
+  base: '/Mywebsite/',
   plugins: [react()],
   resolve: {
     alias: {
