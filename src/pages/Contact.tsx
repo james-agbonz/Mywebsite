e14bccb54dc9463555c05daec19c3d7095d35b23
james-agbonz <153930162+james-agbonz@users.noreply.github.com>
2026-04-26 22:58:05 -0400

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -67,7 +67,7 @@ export default function Contact() {
             <div className="cv-download-bar mb-12">
               <button
                 className="cv-download-button-full"
-                onClick={() => window.open('https://drive.google.com/file/d/1HzvleapdfoFyfYPDgK1aE56r9BFi72w1/view?usp=sharing', '_blank')}
+                onClick={() => window.open('https://drive.google.com/file/d/15fePwLKDb-qgaJ3QjcYXdl8Iu3HVuoY3/view?usp=sharing', '_blank')}
                 aria-label="View Resume"
               >
                 <Download size={24} className="cv-icon" />
